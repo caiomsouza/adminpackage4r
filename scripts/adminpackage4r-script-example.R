152761bf7bc52f09785401552a2489b8ff5171f8
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-06-21 20:37:20 +0200

    Codes of the first version (0.1) of adminpackage4r

---
+++ scripts/adminpackage4r-script-example.R
@@ -0,0 +1,18 @@
+# Install devtools
+install.packages("devtools")
+
+# Load devtools
+library("devtools")
+
+# Install adminpackage4r
+# https://github.com/caiomsouza/adminpackage4r
+devtools::install_github("caiomsouza/adminpackage4r")
+
+#Load adminpackage4r
+library("adminpackage4r")
+
+# Specify the list of required packages to be installed and load    
+Required_Packages=c("ggplot2", "Rcpp", "plyr", "sqldf");
+
+# Call the Function
+Install_And_Load(Required_Packages);
