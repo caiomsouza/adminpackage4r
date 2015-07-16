77911704dede306d9d423803a1e397df39b27781
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-07-16 17:33:50 +0200

    adminpackage4r_install.R

---
+++ adminpackage4r_install.R
@@ -0,0 +1,10 @@
+# https://github.com/caiomsouza/adminpackage4r
+# Install devtools
+install.packages("devtools")
+
+# Load devtools
+library("devtools")
+
+# Install adminpackage4r
+# https://github.com/caiomsouza/adminpackage4r
+devtools::install_github("caiomsouza/adminpackage4r")
